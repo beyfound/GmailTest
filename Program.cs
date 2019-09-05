e2a2023ad862fe96bddbe8a97a515e8d58900a04
derek_qiao <derek.qiao@aspect.com>
2019-09-05 13:56:16 +0800

    delete useless code

--- Program.cs
+++ Program.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace GmailTest
+namespace GmailSender
 {
     class Program
     {
-        static string smtpAddress = "smtp.gmail.com";
-        static int portNumber = 587;
-        static bool enableSSL = true;
-        //static string emailFromAddress = "st.customer01@gmail.com"; //Sender Email Address  
-        //static string password = "Forgerock1"; //Sender Password  
-        //static string emailToAddress = "dopicokoga@gmail.com"; //Receiver Email Address  
-        //static string subject = "Hello";
-        //static string body = "Hello, This is Email sending test using gmail.";
         static void Main(string[] args)
         {
             try
@@ -29,17 +17,15 @@ namespace GmailTest
                     mail.Subject = args[3];
                     mail.Body = args[4];
                     mail.IsBodyHtml = true;
-                    //mail.Attachments.Add(new Attachment("D:\\TestFile.txt"));//--Uncomment this to send any attachment  
-                    using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
                     {
                         smtp.Credentials = new NetworkCredential(args[0], args[1]);
-                        smtp.EnableSsl = enableSSL;
-
+                        smtp.EnableSsl = true;
                         smtp.Send(mail);
 
                     }
                 }
-                Console.WriteLine($"Seccussful");
+                Console.WriteLine("Send mail successfully.");
                 Environment.Exit(0);
             }
             catch (Exception exc)
@@ -47,10 +33,6 @@ namespace GmailTest
                 Console.WriteLine($"Error : {exc.Message}");
                 Environment.Exit(-1);
             }
-        }
-        public static void SendEmail(string emailFromAddress)
-        {
-
         }
     }
 }
